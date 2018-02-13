98b58d412119818123e89d5c707ca99d7b980f73
chen.shen <chen.shen@anyi-tech.com>
2018-02-13 10:20:57 +0800

    correct

--- C/PalindromeNumber/PalindromeNumber.c
+++ C/PalindromeNumber/PalindromeNumber.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 bool isPalindrome(int x) {
-    if(!x || x < 0) {return false;}
+    if(x < 0) {
+        return false;
+    }
+    if(!x) {
+        return true;
+    }
     for(int y = 0; x > y;) {
         y = y * 10 + x % 10;
-        if(!y) {return true;}
-        if(x == y) {return true;}
+        if(!y) {return false;}
+        if(x == y) {
+            return true;
+        }
         x /= 10;
         if(x == y) {return true;}
     }
