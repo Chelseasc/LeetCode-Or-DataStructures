ab60586191805e72f1fd67bc223b10bc06517faa
Chelseasc <sshencchen@sina.com>
2018-03-26 18:06:47 +0800

    romantointerger

--- C/RomanToInteger/romanToInterger.c
+++ C/RomanToInteger/romanToInterger.c
@@ -1,7 +1,6 @@
-#include <stdio.h>
 int romanToInt(char* s) {
-    inline int RTInt(char ch) {
-        switch(ch) {
+    inline int romatoi(char ch) {
+        switch(ch){
             case 'I': return 1;
             case 'X': return 10;
             case 'C': return 100;
@@ -12,12 +11,12 @@ int romanToInt(char* s) {
         }
         return 0;
     }
-    int i = strlen(s) - 1, j = 0, sum = RTInt(s[i]);
-    for(j = i; j > 0; j--) {
-        if(RTInt(s[j] < RTInt(s[j-1]))) {
-            sum -= RTInt(s[j-1]);
+    int i = strlen(s)- 1, j = 0, sum = romatoi(s[i]);
+    for(j = i-1; j >= 0; j--) {
+        if(romatoi(s[i]) > romatoi(s[j])) {
+            sum -= romatoi(s[j]);
         } else {
-            sum += RTInt(s[j-1]);
+            sum += romatoi(s[j]);
         }
     }
     return sum;
