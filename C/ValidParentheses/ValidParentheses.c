5d4fe2b6f78993bc3444aadfa8f7a2ac44fd9f1c
Chelseasc <sshencchen@sina.com>
2018-03-27 18:16:48 +0800

    Valid Parentheses

---
+++ C/ValidParentheses/ValidParentheses.c
@@ -0,0 +1,25 @@
+bool isValid(char* s) {
+    int len = strlen(s);
+    if(len%2 != 0) return false;
+    int lim = len/2;
+    char * stack = malloc(lim);
+    int index = 0;
+    char strpre, strlas;
+    for(int i = 0; i < len; ++i) {
+        strpre = s[i];
+        if(strpre == "(" || strpre == "{" || strpre == "]") {
+            if(index == 0) {
+                stack[index++] = strpre;
+            } else {
+                index--;
+            }
+        } else {
+            strlas = stack[i-1];
+            if((strlas == "(" && strpre == ")")|| (strlas == "{" && strpre == "}")|| (strlas == "[" && strpre == "]")) {
+                index--;
+            }
+        }
+
+    }
+    return index == 0;
+}
