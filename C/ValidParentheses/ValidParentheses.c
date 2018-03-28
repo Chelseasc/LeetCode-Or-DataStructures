5fea99a5bea0cdf6e852a164f4999317af25c7d4
Chelseasc <sshencchen@sina.com>
2018-03-28 14:27:15 +0800

    改bug

--- C/ValidParentheses/ValidParentheses.c
+++ C/ValidParentheses/ValidParentheses.c
@@ -7,13 +7,14 @@ bool isValid(char* s) {
     char strpre, strlas;
     for(int i = 0; i < len; ++i) {
         strpre = s[i];
-        if(strpre == "(" || strpre == "{" || strpre == "]") {
-            if(index == 0) {
-                stack[index++] = strpre;
+        if(strpre == "(" || strpre == "{" || strpre == "[") {
+            if(index == lim) {
+                return false;
             } else {
-                index--;
+                stack[index++] = strpre;
             }
         } else {
+            if(index == 0) return false;
             strlas = stack[i-1];
             if((strlas == "(" && strpre == ")")|| (strlas == "{" && strpre == "}")|| (strlas == "[" && strpre == "]")) {
                 index--;
